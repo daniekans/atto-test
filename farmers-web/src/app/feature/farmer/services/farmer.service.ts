8c46a8f92c3e94799b99bd34a4879136d15a415e
Daniel Franco Lopes <daniel.franco@dtidigital.com.br>
2023-10-07 21:08:07 -0300

    feat(web): adjust environment variable files

--- farmers-web/src/app/feature/farmer/services/farmer.service.ts
+++ farmers-web/src/app/feature/farmer/services/farmer.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { environment } from 'src/environment.dev';
+import { environment } from 'src/environments/environment';
 import { Farmer } from '../models/farmer.interface';
 
 const farmersEndpoint = `${environment.api}/api/farmers`;
@@ -20,8 +20,8 @@ export class FarmerService {
     return this.httpClient.post<Farmer>(farmersEndpoint, farmer);
   }
 
-  updateFarmer(farmer: Farmer): Observable<void> {
-    return this.httpClient.put<void>(`${farmersEndpoint}/${farmer.id}`, farmer);
+  updateFarmer(farmer: Farmer): Observable<Farmer> {
+    return this.httpClient.put<Farmer>(`${farmersEndpoint}/${farmer.id}`, farmer);
   }
 
   deleteFarmer(farmerId: number): Observable<void> {
